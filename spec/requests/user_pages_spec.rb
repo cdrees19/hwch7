79a5a570c624ccc40d32aabef105ba74307f7431
Corey Drees <cdrees@wisc.edu>
2014-04-08 03:04:07 -0500

    finished 9.6.1

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -109,11 +109,14 @@ describe "User pages" do
   end
 
   describe "edit" do
+
     let(:user) { FactoryGirl.create(:user) }
+
     before do
       sign_in user
       visit edit_user_path(user)
     end
+
     before { visit edit_user_path(user) }
 
     describe "page" do
@@ -144,6 +147,18 @@ describe "User pages" do
       it { should have_link('Sign out', href: signout_path) }
       specify { expect(user.reload.name).to  eq new_name }
       specify { expect(user.reload.email).to eq new_email }
+
+      describe "forbidden attributes" do
+        let(:params) do
+          { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+        end
+        before do
+          sign_in user, no_capybara: true
+          patch user_path(user), params
+        end
+        specify { expect(user.reload).not_to be_admin }
+      end
     end
 
   end
