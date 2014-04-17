778f2a747688eab3fdd69b22ad08866a179edf94
Corey Drees <cdrees@wisc.edu>
2014-04-17 18:13:13 -0500

    tried attempting .js

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,6 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = true
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += %w( *.js *.png *.jpg *.jpeg *.gif)
   end
 end
